60c04de0db297e5fe50893a097edb47815845c2f
= <=>
2015-10-01 23:04:05 +0200

    maj parameters + add doctrine-postgis + maj BarController

--- src/AppBundle/Controller/BarController.php
+++ src/AppBundle/Controller/BarController.php
@@ -4,19 +4,23 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Bar;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class BarController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/bars", name="list_bars", )
+     * @Method({"GET"})
      */
-    public function indexAction(Request $request)
+    public function listAction(Request $request)
     {
-
-
+        $this->get('doctrine')->getRepository('AppBundle:Bar')
+            ->findBy(array(
+                ''
+            ));
 
         // replace this example code with whatever you need
         return new JsonResponse(array(
